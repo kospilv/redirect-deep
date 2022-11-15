bbc978dfdea8cd8b5eded07e26a297b25c3955ba
Konstantin Pilyugin <kspilyugin@avito.ru>
2022-11-15 18:32:18 +0300

    let click again

--- index.js
+++ index.js
@@ -8,14 +8,14 @@ window.addEventListener('load', () => {
     console.log('flowRunId', flowRunId);
 
     if (redirectTo) {
-        window.location = redirectTo;
+        // setTimeout(() => window.location = redirectTo, 500);
         // location.assign(redirectTo);
-        // const main = document.querySelector('main');
-        // const linkEl = document.createElement('a');
-        // const linkText = document.createTextNode(redirectTo);
+        const main = document.querySelector('main');
+        const linkEl = document.createElement('a');
+        const linkText = document.createTextNode(redirectTo);
         
-        // linkEl.appendChild(linkText);
-        // main.appendChild(linkEl);
-        // linkEl.setAttribute('href', redirectTo);
+        linkEl.appendChild(linkText);
+        main.appendChild(linkEl);
+        linkEl.setAttribute('href', redirectTo);
     }
 });
